852e8c3cd1fe1d148eb27eb92160752dc964228a
atiffany <abby.tiffany@gmail.com>
2018-04-10 13:34:22 -0700

    bookmarks listed as bullet items

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Bookmark
 
 def index(request):
-    return HttpResponse("You've reached the Bookmarks page!")
+    latest_bookmark_list = Bookmark.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('bookmarks/index.html')
+    context = {
+        'latest_bookmark_list': latest_bookmark_list,
+    }
+    return HttpResponse(template.render(context, request))
