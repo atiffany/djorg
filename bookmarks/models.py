759b14db6e93003ec46c5d2d0462a991f132ab9d
atiffany <abby.tiffany@gmail.com>
2018-04-10 13:03:31 -0700

    created bookmark model

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,13 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Bookmark(models.Model):
+    bookmark_name = models.CharField(max_length=200)
+    bookmark_url = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.bookmark_name
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
