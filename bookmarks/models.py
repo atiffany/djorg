a98de3084c2109cbb073029b05caf95d5a79bda5
atiffany <abby.tiffany@gmail.com>
2018-04-11 15:32:41 -0700

    working on form

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -4,10 +4,16 @@ from django.utils import timezone
 
 class Bookmark(models.Model):
     bookmark_name = models.CharField(max_length=200)
-    bookmark_url = models.CharField(max_length=200)
+    bookmark_url = models.URLField('URL', unique=True)
+    bookmark_note = models.TextField('Note', blank=True)
     pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.bookmark_name
+    def __get_url__(self):
+        return self.bookmark_url
+    def __get_note__(self):
+        return self.bookmark_note
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
--- bookmarks/views.py
+++ bookmarks/views.py
@@ -4,9 +4,12 @@ from django.template import loader
 from .models import Bookmark
 
 def index(request):
-    latest_bookmark_list = Bookmark.objects.order_by('-pub_date')[:5]
+    latest_bookmark_list = Bookmark.objects.all()
     template = loader.get_template('bookmarks/index.html')
     context = {
         'latest_bookmark_list': latest_bookmark_list,
     }
     return HttpResponse(template.render(context, request))
+
+def create(request):
+    return HttpResponse("Working to create a new bookmark.")
