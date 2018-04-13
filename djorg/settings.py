8c31e95ba79ddd200481cb55b042b9fa83f6d598
atiffany <abby.tiffany@gmail.com>
2018-04-13 09:45:07 -0700

    settings tests

--- djorg/settings.py
+++ djorg/settings.py
@@ -13,10 +13,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import dj_database_url
 from decouple import config
-from dotenv import load_dotenv
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
