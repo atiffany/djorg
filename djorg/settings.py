4e15a55f9fb6eccf7eca60ea6175d7e5a3f16b69
atiffany <abby.tiffany@gmail.com>
2018-04-13 10:28:33 -0700

    db settings tests

--- djorg/settings.py
+++ djorg/settings.py
@@ -87,11 +87,12 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=config('DATABASE_URL')
+#     )
+# }
+DATABASES['default'] = dj_database_url.config(default='DATABASE_URL')
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
