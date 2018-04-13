b138bd2a1c6d48b4f0c08500caa366f83befeca2
atiffany <abby.tiffany@gmail.com>
2018-04-13 16:20:40 -0700

    settings test

--- djorg/settings.py
+++ djorg/settings.py
@@ -81,13 +81,13 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-DATABASES = {
+if DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'] = dj_database_url.config(config('DATABASE_URL'))
+else DATABASES['default'] = dj_database_url.config(config('DATABASE_URL'))
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
