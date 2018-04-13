b076ca484c129fb38ceaf27ad7df1847a09de268
atiffany <abby.tiffany@gmail.com>
2018-04-13 09:49:06 -0700

    test settings

--- djorg/settings.py
+++ djorg/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'config(SECRET_KEY)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fierce-bastion-11194.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
