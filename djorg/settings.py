c31d1b12179f3dcddb03fc698170aba0a81a82d6
atiffany <abby.tiffany@gmail.com>
2018-04-16 19:15:15 -0400

    test settings

--- djorg/settings.py
+++ djorg/settings.py
@@ -151,5 +151,5 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
