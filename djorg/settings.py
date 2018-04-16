61b13c4833e2f83186508d5ec3d8ffd3bb627bfd
atiffany <abby.tiffany@gmail.com>
2018-04-16 18:33:37 -0400

    single quotes

--- djorg/settings.py
+++ djorg/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 # DEBUG = True
 
-ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 # ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
